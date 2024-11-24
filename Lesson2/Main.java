9f8d2ef1a058142b017b1b86e0d328af0426d268
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-11-24 13:49:43 +0100

    Klasa circle i konstruktor przeciążony

--- Lesson2/Main.java
+++ Lesson2/Main.java
@@ -4,29 +4,36 @@ class Circle
     //Domyślny konstruktor
     Circle()
     {
-        radius = 1.0;
+        radius = 1.0; //Ustawienie promienia
+        System.out.println("Circle()");
+    }
+    //Konstruktor przeciążony
+    Circle(double r)
+    {
+        radius = r; //Ustawienie promienia na wartość parametru r
+        System.out.println("Circle(double r)");
     }
     //Metody typu setter
     void setRadius(double r)
     {
-        radius = r;
+        radius = r; //Ustawienie promienia na wartość argumentu r
     }
     //Metody typu getter
     double getRadius()
     {
-        return radius;
+        return radius; //Zwracanie wartości promienia
     }
     double getDiameter()
     {
-        return 2 * radius; //Obliczanie średnicy koła
+        return 2 * radius; //Obliczanie średnicy koła i zwracanie wyniku
     }
     double getCircuit()
     {
-        return 2 * 3.14 * radius; //Obliczanie obwodu koła
+        return 2 * 3.14 * radius; //Obliczanie obwodu koła i zwracanie wyniku
     }
     double getArea()
     {
-        return 3.14 * radius * radius; //Obliczanie pola koła
+        return 3.14 * radius * radius; //Obliczanie pola koła i zwracanie wyniku
     }
 }
 
@@ -35,17 +42,25 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Klasa Circle");
-		Circle c1 = new Circle(); //Tworzenie obiektu
+		Circle c1 = new Circle(); //Tworzenie obiektu konstruktorem domyślnym
 		System.out.println("Parametry stworzonego obiektu c1:");
 		System.out.println("promień = " + c1.radius);
 		System.out.println("średnica = " + c1.getDiameter());
 		System.out.println("obwód = " + c1.getCircuit());
 		System.out.println("pole = " + c1.getArea());
 		System.out.println("Parametry zmodyfikowanego obiektu c1:");
-		c1.radius = 2.75;
+		c1.radius = 2.75; //Ustawienie nowego promienia
 		System.out.println("promień = " + c1.radius);
 		System.out.println("średnica = " + c1.getDiameter());
 		System.out.println("obwód = " + c1.getCircuit());
 		System.out.println("pole = " + c1.getArea());
+		System.out.println();
+		Circle c2 = new Circle(7.52); //Tworzenie obiektu konstruktorem przeciążonym
+		System.out.println("Parametry stworzonego obiektu c2:");
+		System.out.println("promień = " + c2.radius);
+		System.out.println("średnica = " + c2.getDiameter());
+		System.out.println("obwód = " + c2.getCircuit());
+		System.out.println("pole = " + c2.getArea());
+		System.out.println();
 	}
 }
